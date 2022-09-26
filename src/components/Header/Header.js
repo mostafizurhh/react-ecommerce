bc89f7fbda3a8bf815a073119389fa5a531e47cc
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-26 02:34:58 +0100

    project done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,13 @@ const Header = () => {
     return (
         <div >
             <nav className='header'>
-                <img src={logo} alt="" />
                 <div>
+                    <img src={logo} alt="" />
+                </div>
+                <div >
                     <a href="/order">Order</a>
-                    <a href="/order-review">Order Review</a>
-                    <a href="/manage-inventory">Manage Inventory</a>
+                    <a href="/order-review">Order-Review</a>
+                    <a href="/manage-inventory">Manage-Inventory</a>
                     <a href="/login">Login</a>
                 </div>
             </nav>
---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const Product = (props) => {
+    const { img, name, price, seller, ratings } = props.product
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <p>{name}</p>
+            <p>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Rating: {ratings}</p>
+            <button onClick={() => props.handleAddToCart(props.product.id)} className='btn-cart'>
+                <p>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
+        </div>
+    );
+};
+
+export default Product;

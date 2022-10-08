bc33933c2770cb41a2cb906840590071ae06da07
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-08 08:20:48 +0100

    add click handler function on delete button for removing item from cart and database

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,29 +1,28 @@
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './OrderReview.css'
 
-const OrderReview = ({ product }) => {
-    const { name, price, quantity, img, shipping } = product;
+const OrderReview = ({ product, handleRemoveItemFromCart }) => {
+    const { id, name, price, quantity, img, shipping } = product;
     return (
-        <div className='review-order'>
-            <div className="review-order-details">
-                <div className='cart-item-details'>
-                    <div>
-                        <img src={img} alt="" />
-                    </div>
-                    <div className='cart-item-info'>
-                        <p>{name}</p>
-                        <p><small>Price: ${price}</small></p>
-                        <p><small>Quantity: {quantity}</small></p>
-                        <p><small>Shipping Charge: ${shipping}</small></p>
-                    </div>
-                </div>
+        <div className="review-order-details">
+            <div className='cart-item-details'>
                 <div>
-                    <button>Delete</button>
+                    <img src={img} alt="" />
+                </div>
+                <div className='cart-item-info'>
+                    <p>{name}</p>
+                    <p><small>Price: ${price}</small></p>
+                    <p><small>Quantity: {quantity}</small></p>
+                    <p><small>Shipping Charge: ${shipping}</small></p>
                 </div>
             </div>
-
-
-
+            <div>
+                <button onClick={() => handleRemoveItemFromCart(id)}>
+                    <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+                </button>
+            </div>
         </div>
     );
 };
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import OrderReview from '../OrderReview/OrderReview';
 
 const Orders = () => {
     const { products, initialCart } = useLoaderData()
     const [cart, setCart] = useState(initialCart) //using useState() as we want to remove any item from cart
+
+    const handleRemoveItemFromCart = id => {
+        const remainingItemInCart = cart.filter(product => product.id !== id);
+        setCart(remainingItemInCart)
+        removeFromDb(id)
+    }
     return (
         <div className='shop-container'>
             <div className='orders-container'>
@@ -13,6 +20,7 @@ const Orders = () => {
                     cart.map(product => <OrderReview
                         key={product.id}
                         product={product}
+                        handleRemoveItemFromCart={handleRemoveItemFromCart}
                     ></OrderReview>)
                 }
 

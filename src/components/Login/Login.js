f9ceb5a82ae016db187e57f01bf0b14b9326655b
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-16 20:53:14 +0100

    login and register form created with button toggling option

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>login</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content ">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label mt-2">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+
+                            {/* toggle button */}
+                            <label className="label mt-2">
+                                <small>New to our website?</small>
+                                <Link to="/register" className="label-text-alt link link-hover ml-2">Register here!</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-3">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
